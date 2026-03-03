0129b4d8bf51463ddec173da1373cfa985dda530
= <=>
2026-03-03 11:35:58 -0300

    feat(formAdd): criando e ajustando o botão de "voltar" no formulário "add", "edit" e "delete" do cardápio

--- frontend/src/components/FormCrudCardapio.tsx
+++ frontend/src/components/FormCrudCardapio.tsx
@@ -6,30 +6,35 @@ interface CardapioAdminProps {
 
 
 export default function CardapioAdmin({ type }: CardapioAdminProps) {
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <>
             {type === 'add' ? (
                 <>
-                    <header className="text-black p-4 text-2xl font-bold mt-7 text-center">
-                        <button onClick={() => navigate(-1)} className="bg-gray-500 p-3 hover:bg-gray-600 cursor-pointer text-white rounded-md">Voltar</button>
-                        <p className="inline-block ml-4">Adicionar Item ao Cardápio</p>
+                    <header className="relative flex flex-1 flex-col items-center justify-center text-black p-4 text-2xl font-bold mt-5">
+                        <button
+                            onClick={() => navigate(-1)}
+                            className="md:absolute left-4 bg-gray-500 p-3 sm:mb-2 hover:bg-gray-600 cursor-pointer text-white text-sm font-normal rounded-md"
+                        >
+                            Voltar
+                        </button>
+                        <p>Adicionar Item ao Cardápio</p>
                     </header>
                     <main>
                         <div className="max-w-md mx-auto bg-gray-200 p-6 rounded-lg shadow-lg m-8">
-                            <form className="max-w-md mx-auto bg-white p-8 rounded-md shadow-md" encType="multipart/form-data">
+                            <form className="max-w-md mx-auto bg-white p-8 rounded-md shadow-md text-center" encType="multipart/form-data">
                                 <label htmlFor="nome">Nome do Item:</label>
-                                <input className="border border-gray-300 rounded-md p-2 w-full" type="text" id="nome" name="nome" required />
+                                <input className="border mt-2 mb-2 border-gray-300 rounded-md p-2 w-full" type="text" id="nome" name="nome" required />
 
                                 <label htmlFor="descricao">Descrição do Item:</label>
-                                <input className="h-30 border border-gray-300 rounded-md p-2 w-full" id="descricao" name="descricao" required></input>
+                                <input className="h-30 mt-2 mb-2 border border-gray-300 rounded-md p-2 w-full" id="descricao" name="descricao" required></input>
 
                                 <label htmlFor="preco">Preço do Item:</label>
-                                <input className="border border-gray-300 rounded-md p-2 w-full" type="number" id="preco" name="preco" required />
+                                <input className="border mt-2 mb-2 border-gray-300 rounded-md p-2 w-full" type="number" id="preco" name="preco" required />
 
                                 <label htmlFor="categoria">Categoria do Item:</label>
-                                <select className="border border-gray-300 rounded-md p-2 w-full" id="categoria" name="categoria" required>
+                                <select className="border mt-2 mb-2 border-gray-300 rounded-md p-2 w-full" id="categoria" name="categoria" required>
                                     <option value="">Selecione uma categoria</option>
                                     <option value="entrada">Entrada</option>
                                     <option value="prato_principal">Prato Principal</option>
@@ -37,8 +42,8 @@ const navigate = useNavigate();
                                     <option value="bebida">Bebida</option>
                                 </select>
 
-                                <label htmlFor="imagem">Adicione imagens ao item</label>
-                                <input className="border border-gray-300 rounded-md p-2 w-full" type="file" id="imagem" name="imagem" accept="image/*" multiple />
+                                <label htmlFor="imagem">Adicionar imagem</label>
+                                <input className="border mt-2 mb-2 border-gray-300 rounded-md p-2 w-full" type="file" id="imagem" name="imagem" accept="image/*" multiple />
 
                                 <button className="bg-green-500 text-white px-4 py-2 rounded-md mt-4" type="submit">Adicionar Item</button>
                             </form>
@@ -47,8 +52,14 @@ const navigate = useNavigate();
                 </>
             ) : (
                 <>
-                    <header className="text-black p-4 text-2xl font-bold text-center mt-7">
-                        <p>Editar Item do Cardápio</p>
+                    <header className="relative flex flex-1 flex-col items-center justify-center text-black p-4 text-2xl font-bold mt-5">
+                        <button
+                            onClick={() => navigate(-1)}
+                            className="md:absolute left-4 bg-gray-500 p-3 sm:mb-2 hover:bg-gray-600 cursor-pointer text-white text-sm font-normal rounded-md"
+                        >
+                            Voltar
+                        </button>
+                        <p>Editar Cardápio</p>
                     </header>
                     <main>
                         <form>
--- frontend/src/views/admin/cardapio/CardapioExcluir.tsx
+++ frontend/src/views/admin/cardapio/CardapioExcluir.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function CardapioExcluir() {
+
+    const navigate = useNavigate();
     return (
         <>
-            <header className="text-black p-4 text-2xl font-bold text-center mt-7">
+            <header className="relative flex flex-1 flex-col items-center justify-center text-black p-4 text-2xl font-bold mt-5">
+                <button
+                    onClick={() => navigate(-1)}
+                    className="md:absolute left-4 bg-gray-500 p-3 sm:mb-2 hover:bg-gray-600 cursor-pointer text-white text-sm font-normal rounded-md"
+                >
+                    Voltar
+                </button>
                 <p>Excluir Item do Cardápio</p>
             </header>
         </>
