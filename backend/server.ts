af34662e4ca3eafd2e08fc0df3e4fe1b0282882a
Gustavo Souza <cavalogames1231@gmail.com>
2026-03-07 10:16:15 -0300

    fix(docker): ajustando as arquiteturas para funcionamento do docker.

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,10 @@ fastify.register(cors, {
 
 fastify.register(userRoutes);
 
-fastify.listen({ port: 3000 }, err => {
+fastify.listen({ 
+    port: 3000,
+    host: '0.0.0.0',
+ }, err => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
